47f56fadb005caee63f2275a097b935257b5e1ed
kcastagni <katherinecastagnoli@gmail.com>
2026-02-23 22:42:40 -0300

    html actualizada

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+console.log('Estamos conectados');
+
+/*
+----------------------------------
+configuración
+----------------------------------
+*/
+
+const IVA = 0.19
+
+/*
+----------------------------------
+
+----------------------------------
+*/
+
+let inventario = [
+
+];
+
+
+/*
+----------------------------------
+
+----------------------------------
+*/
+
+function agregarProducto(){
+    
+}
+
+/*
+----------------------------------
+Referencias del DOM
+----------------------------------
+*/
+
+const tablaProductos = document.querySelector('#tabla-productos');
+const valorTotal = document.querySelector('#valor-total');
+
+
+
+
+
+/*
+----------------------------------
+Render del DOM
+----------------------------------
+*/
